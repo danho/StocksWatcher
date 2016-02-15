ce1175575e32b27ca831d3dd6d4f666a3a6293c2
Dan <devdannyho@gmail.com>
2016-02-15 17:42:11 -0500

    added more triggers

--- server/server.js
+++ server/server.js
@@ -21,8 +21,22 @@
             if (d.Ask <= dict[d.symbol].lowTarget) {
                 console.log(d.symbol, 'lower than low target with a price of ', d.Ask);
             }
+
             if (d.Ask <= d.YearRange.split(' ')[0]) {
                 console.log(d.symbol, 'lower than 52 week low with a price of', d.Ask);
+            }
+            else if (d.Ask >= d.YearRange.split(' ')[2]) {
+                console.log(d.symbol, 'higher than 52 week high with a price of', d.Ask);
+            }
+
+            if (parseInt(d.ChangeinPercent) >= 20) {
+                console.log(d.symbol, 'has a change of more than 20 percent with a price of', d.Ask);
+            }
+            else if (parseInt(d.ChangeinPercent) >= 10) {
+                console.log(d.symbol, 'has a change of more than 10 percent with a price of', d.Ask);
+            }
+            else if (parseInt(d.ChangeinPercent) >= 5) {
+                console.log(d.symbol, 'has a change of more than 5 percent with a price of', d.Ask);
             }
         });
     }
